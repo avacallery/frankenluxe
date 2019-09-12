190b308e21917f8a3f15327c43c0b2a84b670f30
avacallery <avacallery@gmail.com>
2019-09-12 12:50:10 -0400

    DOM Content Loaded

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-var list = document.querySelector('#drink-list ul'); 
-var forms = document.forms; 
+document.addEventListener('DOMContentLoaded', function(){
+    var list = document.querySelector('#drink-list ul'); 
+    var forms = document.forms;   
+    
+});
+
+
 
