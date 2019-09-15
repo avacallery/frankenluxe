1b538ffb0b8a6d20aadf04e2d7c4bb3b607b777f
avacallery <avacallery@gmail.com>
2019-09-15 19:08:09 -0400

    add delete drinks/add drinks

--- script.js
+++ script.js
@@ -4,5 +4,35 @@ document.addEventListener('DOMContentLoaded', function(){
     
 });
 
+//delete drinks
+list.addEventListener('click', (e) => {
+    if (e.target.className == 'delete'){
+        var li = e.target.parentElement; 
+        li.parentNode.removeChild(li); 
+    }
+}); 
 
+//add drinks
+var addForm = forms['add-drink'];
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
 
+//create elements
+var value = addForm.querySelector('input[type="text"]').value;
+var li = document.createElement('li');
+var drinkName = document.createElement('span');
+var deleteButton = document.createElement('span');
+
+//add text content
+drinkName.textContent = value;
+deleteButton.textContent = 'delete'; 
+
+//add classes
+drinkName.classList.add('name');
+deleteButton.classList.add('delete');
+
+//append to DOM
+li.appendChild(drinkName);
+li.appendChild(deleteButton);
+list.appendChild(li); 
+});
